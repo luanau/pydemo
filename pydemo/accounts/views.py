cbadb7589140d38269d204de824b710b4bfaabbe
luanau <luanau@gmail.com>
2014-03-12 13:16:40 +0000

    Second commit

--- pydemo/accounts/views.py
+++ pydemo/accounts/views.py
@@ -6,4 +6,5 @@ def my_view(request):
     a = prime(1,19)
     b = 'string'
     c = 2
+    d = 'string'
     return render_to_response('index.html', locals(), context_instance = RequestContext(request))

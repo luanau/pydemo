98395a4cb5c74a3fadbf31eac7e754cf7d23447b
luanau <luanau@gmail.com>
2014-03-13 23:36:24 +0000

    Add and tried some tests

--- pydemo/accounts/tests.py
+++ pydemo/accounts/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+#from django.test import TestCase
+#from django.test.simple.DjangoTestSuiteRunner import 
+import unittest
+
+def my_func(a, index):
+    return a[index]
 
 # Create your tests here.
+class MyFuncTestCase(unittest.TestCase):
+    def testBasic(self):
+        a = ['larry', 'curly', 'moe']
+        self.assertEqual(my_func(a, 0), 'larry')
+        self.assertEqual(my_func(a, 1), 'curly')
--- pydemo/pydemo/urls.py
+++ pydemo/pydemo/urls.py
@@ -5,9 +5,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'pydemo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    # url(r'^$', 'pydemo.views.home', name='home'),
+    url(r'^$', 'pydemo.views.home_page', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^demo/', 'accounts.views.my_view'),
 )
